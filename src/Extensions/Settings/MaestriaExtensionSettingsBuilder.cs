bb2c85a59929dcc5549b958a7b76a2a2276ccda0
Fábio Naspolini <fabionaspolini@gmail.com>
2019-10-07 23:04:55 -0300

    Extensions methods

---
+++ src/Extensions/Settings/MaestriaExtensionSettingsBuilder.cs
@@ -0,0 +1,18 @@
+namespace Maestria.Extensions.Settings
+{
+    public class MaestriaExtensionSettingsBuilder
+    {
+        private readonly MaestriaExtensionSettings _instance;
+
+        public MaestriaExtensionSettingsBuilder(MaestriaExtensionSettings instance)
+        {
+            _instance = instance;
+        }
+
+        public MaestriaExtensionSettingsBuilder FloatAndDoubleTolerance(float value)
+        {
+            _instance.FloatAndDoubleTolerance = value;
+            return this;
+        }
+    }
+}
