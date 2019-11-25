af72777c6c1ad253063a81c36be69e9abec6daa9
Fábio Naspolini <fabionaspolini@gmail.com>
2019-11-25 01:02:19 -0300

    Round test

--- src/Extensions/RoundExtensions.cs
+++ src/Extensions/RoundExtensions.cs
@@ -6,63 +6,81 @@ namespace Maestria.Extensions
     {
         #region Round float
 
-        public static float Round(this float value) => value.Round(0);
-
-        public static float Round(this float value, int digits) =>
-            Convert.ToSingle(Math.Round(Convert.ToDecimal(value), digits));
+        public static float Round(this float value, int digits = 0) =>
+            Convert.ToSingle(Convert.ToDecimal(value).Round(digits));
 
         public static float Round(this float value, int digits, MidpointRounding mode) =>
-            Convert.ToSingle(Math.Round(Convert.ToDecimal(value), digits, mode));
+            Convert.ToSingle(Convert.ToDecimal(value).Round(digits, mode));
         
+        public static float RoundDown(this float value, int digits = 0) =>
+            Convert.ToSingle(Convert.ToDecimal(value).RoundDown(digits));
+
+        public static float RoundUp(this float value, int digits = 0) =>
+            Convert.ToSingle(Convert.ToDecimal(value).RoundUp(digits));
+
         #endregion
         
         #region Round double
-
-        public static double Round(this double value) => value.Round(0);
-
-        public static double Round(this double value, int digits) =>
-            Convert.ToDouble(Math.Round(Convert.ToDecimal(value), digits));
+        
+        public static double Round(this double value, int digits = 0) =>
+            Convert.ToDouble(Convert.ToDecimal(value).Round(digits));
 
         public static double Round(this double value, int digits, MidpointRounding mode) =>
-            Convert.ToDouble(Math.Round(Convert.ToDecimal(value), digits, mode));
-        
+            Convert.ToDouble(Convert.ToDecimal(value).Round(digits, mode));
+
+        public static double RoundDown(this double value, int digits = 0) =>
+            Convert.ToDouble(Convert.ToDecimal(value).RoundDown(digits));
+
+        public static double RoundUp(this double value, int digits = 0) =>
+            Convert.ToDouble(Convert.ToDecimal(value).RoundUp(digits));
+
         #endregion
         
         #region Round decimal
-
-        public static decimal Round(this decimal value) => value.Round(0);
-        public static decimal Round(this decimal value, int digits) => Math.Round(value, digits);
+        
+        public static decimal Round(this decimal value, int digits = 0) => Math.Round(value, digits);
 
         public static decimal Round(this decimal value, int digits, MidpointRounding mode) =>
             Math.Round(value, digits, mode);
+        
+        public static decimal RoundDown(this decimal value, int digits = 0) =>
+            Math.Floor(value * Convert.ToDecimal(Math.Pow(10, digits))) / Convert.ToDecimal(Math.Pow(10, digits));
+
+        public static decimal RoundUp(this decimal value, int digits = 0) =>
+            Math.Ceiling(value * Convert.ToDecimal(Math.Pow(10, digits))) / Convert.ToDecimal(Math.Pow(10, digits));
 
         #endregion
 
         #region Round nullable float
-
-        public static float? Round(this float? value) => value?.Round();
-        public static float? Round(this float? value, int digits) => value?.Round(digits);
+        
+        public static float? Round(this float? value, int digits = 0) => value?.Round(digits);
         public static float? Round(this float? value, int digits, MidpointRounding mode) => value?.Round(digits, mode);
+        public static float? RoundDown(this float? value, int digits = 0) => value?.RoundDown(digits);
+        public static float? RoundUp(this float? value, int digits = 0) => value?.RoundUp(digits);
         
         #endregion
         
         #region Round nullable double
-
-        public static double? Round(this double? value) => value?.Round();
-        public static double? Round(this double? value, int digits) => value?.Round(digits);
+        
+        public static double? Round(this double? value, int digits = 0) => value?.Round(digits);
 
         public static double? Round(this double? value, int digits, MidpointRounding mode) =>
             value?.Round(digits, mode);
         
+        public static double? RoundDown(this double? value, int digits = 0) => value?.RoundDown(digits);
+        public static double? RoundUp(this double? value, int digits = 0) => value?.RoundUp(digits);
+        
         #endregion
         
         #region Round nullable decimal
-
-        public static decimal? Round(this decimal? value) => value?.Round();
-        public static decimal? Round(this decimal? value, int digits) => value?.Round(digits);
+        
+        public static decimal? Round(this decimal? value, int digits = 0) => value?.Round(digits);
 
         public static decimal? Round(this decimal? value, int digits, MidpointRounding mode) =>
             value?.Round(digits, mode);
+
+        public static decimal? RoundDown(this decimal? value, int digits = 0) => value?.RoundDown(digits);
+        public static decimal? RoundUp(this decimal? value, int digits = 0) => value?.RoundUp(digits);
 
         #endregion
     }
--- tests/Extensions.Test.CSharp/RoundExtensionsTest.cs
+++ tests/Extensions.Test.CSharp/RoundExtensionsTest.cs
@@ -25,7 +25,31 @@ namespace Maestria.Extensions.Test.CSharp
             
             Assert.AreEqual(1.23f, 1.225f.Round(2, MidpointRounding.AwayFromZero));
             Assert.AreEqual(1.24f, 1.235f.Round(2, MidpointRounding.AwayFromZero));
-        } 
+        }
+        
+        [Test]
+        public void RoundDown()
+        {
+            Assert.AreEqual(1f, 1f.RoundDown());
+            Assert.AreEqual(1f, 1.225f.RoundDown());
+            Assert.AreEqual(1.22f, 1.22f.RoundDown(2));
+            Assert.AreEqual(1.22f, 1.225f.RoundDown(2));
+            Assert.AreEqual(1.23f, 1.235f.RoundDown(2));
+            Assert.AreEqual(1.23f, 1.231f.RoundDown(2));
+            Assert.AreEqual(1.23f, 1.239f.RoundDown(2));
+        }
+        
+        [Test]
+        public void RoundUp()
+        {
+            Assert.AreEqual(1f, 1f.RoundUp());
+            Assert.AreEqual(2f, 1.225f.RoundUp());
+            Assert.AreEqual(1.22f, 1.22f.RoundUp(2));
+            Assert.AreEqual(1.23f, 1.225f.RoundUp(2));
+            Assert.AreEqual(1.24f, 1.235f.RoundUp(2));
+            Assert.AreEqual(1.24f, 1.231f.RoundUp(2));
+            Assert.AreEqual(1.24f, 1.239f.RoundUp(2));
+        }
     }
     
     public class RoundExtensions_DoubleTest
@@ -47,7 +71,31 @@ namespace Maestria.Extensions.Test.CSharp
             
             Assert.AreEqual(1.23d, 1.225d.Round(2, MidpointRounding.AwayFromZero));
             Assert.AreEqual(1.24d, 1.235d.Round(2, MidpointRounding.AwayFromZero));
-        } 
+        }
+        
+        [Test]
+        public void RoundDown()
+        {
+            Assert.AreEqual(1d, 1d.RoundDown());
+            Assert.AreEqual(1d, 1.225d.RoundDown());
+            Assert.AreEqual(1.22d, 1.22d.RoundDown(2));
+            Assert.AreEqual(1.22d, 1.225d.RoundDown(2));
+            Assert.AreEqual(1.23d, 1.235d.RoundDown(2));
+            Assert.AreEqual(1.23d, 1.231d.RoundDown(2));
+            Assert.AreEqual(1.23d, 1.239d.RoundDown(2));
+        }
+        
+        [Test]
+        public void RoundUp()
+        {
+            Assert.AreEqual(1d, 1d.RoundUp());
+            Assert.AreEqual(2d, 1.225d.RoundUp());
+            Assert.AreEqual(1.22d, 1.22d.RoundUp(2));
+            Assert.AreEqual(1.23d, 1.225d.RoundUp(2));
+            Assert.AreEqual(1.24d, 1.235d.RoundUp(2));
+            Assert.AreEqual(1.24d, 1.231d.RoundUp(2));
+            Assert.AreEqual(1.24d, 1.239d.RoundUp(2));
+        }
     }
 
     public class RoundExtensions_DecimalTest
@@ -69,7 +117,31 @@ namespace Maestria.Extensions.Test.CSharp
             
             Assert.AreEqual(1.23m, 1.225m.Round(2, MidpointRounding.AwayFromZero));
             Assert.AreEqual(1.24m, 1.235m.Round(2, MidpointRounding.AwayFromZero));
-        } 
+        }
+
+        [Test]
+        public void RoundDown()
+        {
+            Assert.AreEqual(1m, 1m.RoundDown());
+            Assert.AreEqual(1m, 1.225m.RoundDown());
+            Assert.AreEqual(1.22m, 1.22m.RoundDown(2));
+            Assert.AreEqual(1.22m, 1.225m.RoundDown(2));
+            Assert.AreEqual(1.23m, 1.235m.RoundDown(2));
+            Assert.AreEqual(1.23m, 1.231m.RoundDown(2));
+            Assert.AreEqual(1.23m, 1.239m.RoundDown(2));
+        }
+        
+        [Test]
+        public void RoundUp()
+        {
+            Assert.AreEqual(1m, 1m.RoundUp());
+            Assert.AreEqual(2m, 1.225m.RoundUp());
+            Assert.AreEqual(1.22m, 1.22m.RoundUp(2));
+            Assert.AreEqual(1.23m, 1.225m.RoundUp(2));
+            Assert.AreEqual(1.24m, 1.235m.RoundUp(2));
+            Assert.AreEqual(1.24m, 1.231m.RoundUp(2));
+            Assert.AreEqual(1.24m, 1.239m.RoundUp(2));
+        }
     }
     
     #endregion
@@ -95,7 +167,31 @@ namespace Maestria.Extensions.Test.CSharp
             
             Assert.AreEqual(1.23f, new float?(1.225f).Round(2, MidpointRounding.AwayFromZero));
             Assert.AreEqual(1.24f, new float?(1.235f).Round(2, MidpointRounding.AwayFromZero));
-        } 
+        }
+        
+        [Test]
+        public void RoundDown()
+        {
+            Assert.AreEqual(1f, new float?(1f).RoundDown());
+            Assert.AreEqual(1f, new float?(1.225f).RoundDown());
+            Assert.AreEqual(1.22f, new float?(1.22f).RoundDown(2));
+            Assert.AreEqual(1.22f, new float?(1.225f).RoundDown(2));
+            Assert.AreEqual(1.23f, new float?(1.235f).RoundDown(2));
+            Assert.AreEqual(1.23f, new float?(1.231f).RoundDown(2));
+            Assert.AreEqual(1.23f, new float?(1.239f).RoundDown(2));
+        }
+        
+        [Test]
+        public void RoundUp()
+        {
+            Assert.AreEqual(1f, new float?(1f).RoundUp());
+            Assert.AreEqual(2f, new float?(1.225f).RoundUp());
+            Assert.AreEqual(1.22f, new float?(1.22f).RoundUp(2));
+            Assert.AreEqual(1.23f, new float?(1.225f).RoundUp(2));
+            Assert.AreEqual(1.24f, new float?(1.235f).RoundUp(2));
+            Assert.AreEqual(1.24f, new float?(1.231f).RoundUp(2));
+            Assert.AreEqual(1.24f, new float?(1.239f).RoundUp(2));
+        }
     }
     
     public class RoundExtensions_NullableDoubleTest
@@ -117,7 +213,31 @@ namespace Maestria.Extensions.Test.CSharp
             
             Assert.AreEqual(1.23d, new double?(1.225d).Round(2, MidpointRounding.AwayFromZero));
             Assert.AreEqual(1.24d, new double?(1.235d).Round(2, MidpointRounding.AwayFromZero));
-        } 
+        }
+        
+        [Test]
+        public void RoundDown()
+        {
+            Assert.AreEqual(1d, new double?(1d).RoundDown());
+            Assert.AreEqual(1d, new double?(1.225d).RoundDown());
+            Assert.AreEqual(1.22d, new double?(1.22d).RoundDown(2));
+            Assert.AreEqual(1.22d, new double?(1.225d).RoundDown(2));
+            Assert.AreEqual(1.23d, new double?(1.235d).RoundDown(2));
+            Assert.AreEqual(1.23d, new double?(1.231d).RoundDown(2));
+            Assert.AreEqual(1.23d, new double?(1.239d).RoundDown(2));
+        }
+        
+        [Test]
+        public void RoundUp()
+        {
+            Assert.AreEqual(1d, new double?(1d).RoundUp());
+            Assert.AreEqual(2d, new double?(1.225d).RoundUp());
+            Assert.AreEqual(1.22d, new double?(1.22d).RoundUp(2));
+            Assert.AreEqual(1.23d, new double?(1.225d).RoundUp(2));
+            Assert.AreEqual(1.24d, new double?(1.235d).RoundUp(2));
+            Assert.AreEqual(1.24d, new double?(1.231d).RoundUp(2));
+            Assert.AreEqual(1.24d, new double?(1.239d).RoundUp(2));
+        }
     }
 
     public class RoundExtensions_NullableDecimalTest
@@ -140,6 +260,30 @@ namespace Maestria.Extensions.Test.CSharp
             Assert.AreEqual(1.23m, new decimal?(1.225m).Round(2, MidpointRounding.AwayFromZero));
             Assert.AreEqual(1.24m, new decimal?(1.235m).Round(2, MidpointRounding.AwayFromZero));
         } 
+        
+        [Test]
+        public void RoundDown()
+        {
+            Assert.AreEqual(1m, new decimal?(1m).RoundDown());
+            Assert.AreEqual(1m, new decimal?(1.225m).RoundDown());
+            Assert.AreEqual(1.22m, new decimal?(1.22m).RoundDown(2));
+            Assert.AreEqual(1.22m, new decimal?(1.225m).RoundDown(2));
+            Assert.AreEqual(1.23m, new decimal?(1.235m).RoundDown(2));
+            Assert.AreEqual(1.23m, new decimal?(1.231m).RoundDown(2));
+            Assert.AreEqual(1.23m, new decimal?(1.239m).RoundDown(2));
+        }
+        
+        [Test]
+        public void RoundUp()
+        {
+            Assert.AreEqual(1m, new decimal?(1m).RoundUp());
+            Assert.AreEqual(2m, new decimal?(1.225m).RoundUp());
+            Assert.AreEqual(1.22m, new decimal?(1.22m).RoundUp(2));
+            Assert.AreEqual(1.23m, new decimal?(1.225m).RoundUp(2));
+            Assert.AreEqual(1.24m, new decimal?(1.235m).RoundUp(2));
+            Assert.AreEqual(1.24m, new decimal?(1.231m).RoundUp(2));
+            Assert.AreEqual(1.24m, new decimal?(1.239m).RoundUp(2));
+        }
     }
     
     #endregion
