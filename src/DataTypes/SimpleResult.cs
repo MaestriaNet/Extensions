b88b8adc73c4d933e5caf42ccf4f2b4c50e25b6b
Fábio Naspolini <fabio.naspolini@bbmlogistica.com.br>
2021-01-10 19:22:30 -0300

    Implicit operator SimpleResult<TData> by Data

--- src/DataTypes/SimpleResult.cs
+++ src/DataTypes/SimpleResult.cs
@@ -38,10 +38,12 @@
         public string Message { get; set; }
         public TData Data { get; set; }
 
+        public static SimpleResult<TData> Ok(TData data, string message = null) => new SimpleResult<TData> { Success = true, Message = message, Data = data };
         public static SimpleResult<TData> Ok(string message = null) => new SimpleResult<TData> { Success = true, Message = message };
         public static SimpleResult<TData> Fail(string message) => new SimpleResult<TData> { Success = false, Message = message };
 
         public static implicit operator SimpleResult<TData>(bool success) => new SimpleResult<TData> { Success = success };
+        public static implicit operator SimpleResult<TData>(TData data) => new SimpleResult<TData> { Success = true, Data = data };
         public static implicit operator SimpleResult<TData>(string failMessage) => Fail(failMessage);
         public static implicit operator bool(SimpleResult<TData> value) => value.Success;
         public static implicit operator TData(SimpleResult<TData> value) => value.Data;
