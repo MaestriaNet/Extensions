9904b1f7367cd53d41b0ab9ed4811e35b95dbc2e
Fábio Naspolini <fabionaspolini@gmail.com>
2025-11-14 00:13:41 -0300

    refactor: adaptar código de create hasher obsoleto

--- src/Extensions/String/Hash/CreateHasher.cs
+++ src/Extensions/String/Hash/CreateHasher.cs
@@ -7,11 +7,11 @@ public static partial class MaestriaExtensions
 {
     public static System.Security.Cryptography.HashAlgorithm CreateHasher(this HashAlgorithm value) => value switch
     {
-        HashAlgorithm.Md5 => new MD5CryptoServiceProvider(),
-        HashAlgorithm.Sha1 => new SHA1Managed(),
-        HashAlgorithm.Sha256 => new SHA256Managed(),
-        HashAlgorithm.Sha384 => new SHA384Managed(),
-        HashAlgorithm.Sha512 => new SHA512Managed(),
-        _ => throw new ArgumentOutOfRangeException(nameof(value)),
+        HashAlgorithm.Md5 => MD5.Create(),
+        HashAlgorithm.Sha1 => SHA1.Create(),
+        HashAlgorithm.Sha256 => SHA256.Create(),
+        HashAlgorithm.Sha384 => SHA384.Create(),
+        HashAlgorithm.Sha512 => SHA512.Create(),
+        _ => throw new ArgumentOutOfRangeException(nameof(value), value, "Unsupported hash algorithm."),
     };
 }
