987bce6d67c60d47d231d774d8cb98cb962256ec
Fábio Naspolini <fabionaspolini@gmail.com>
2026-07-17 15:08:26 -0300

    refactor: enhance hashing methods with encoding support
    - Added optional encoding parameter to ComputeHash methods.
    - Implemented hashing logic for .NET 5.0 and greater using built-in hash functions.
    - Created a dictionary to manage hash algorithm instances efficiently.
    - Updated MaestriaExtensionSettings to include default encoding property.

--- src/Extensions/String/Hash/ComputeHash.cs
+++ src/Extensions/String/Hash/ComputeHash.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Maestria.Extensions;
@@ -10,15 +11,30 @@ public static partial class MaestriaExtensions
     /// </summary>
     /// <param name="algorithm"></param>
     /// <param name="value"></param>
+    /// <param name="encoding"></param>
     /// <returns></returns>
     /// <exception cref="ArgumentNullException"></exception>
-    public static string ComputeHash(this string value, HashAlgorithm algorithm)
+    public static string ComputeHash(this string value, HashAlgorithm algorithm, Encoding? encoding = null)
     {
         if (value == null)
-            throw new ArgumentNullException(nameof(value), "Null value to encrypt not suported.");
+            throw new ArgumentNullException(nameof(value), "Null value to encrypt not supported.");
 
-        using var hasher = algorithm.CreateHasher();
-        return value.ComputeHash(hasher);
+        encoding ??= GlobalSettings.Properties.DefaultEncoding;
+
+#if NET5_0_OR_GREATER
+        var hashBytes = algorithm switch
+        {
+            HashAlgorithm.Md5 => MD5.HashData(encoding.GetBytes(value)),
+            HashAlgorithm.Sha1 => SHA1.HashData(encoding.GetBytes(value)),
+            HashAlgorithm.Sha256 => SHA256.HashData(encoding.GetBytes(value)),
+            HashAlgorithm.Sha384 => SHA384.HashData(encoding.GetBytes(value)),
+            HashAlgorithm.Sha512 => SHA512.HashData(encoding.GetBytes(value)),
+            _ => throw new ArgumentOutOfRangeException(nameof(algorithm)),
+        };
+        return hashBytes.HashBytesToString();
+#else
+        return value.ComputeHash(algorithm.GetHasher(), encoding);
+#endif
     }
 
     /// <summary>
@@ -26,15 +42,22 @@ public static partial class MaestriaExtensions
     /// </summary>
     /// <param name="algorithm"></param>
     /// <param name="value"></param>
+    /// <param name="encoding"></param>
     /// <returns></returns>
     /// <exception cref="ArgumentNullException"></exception>
-    public static string ComputeHash(this string value, System.Security.Cryptography.HashAlgorithm algorithm)
+    public static string ComputeHash(this string value, System.Security.Cryptography.HashAlgorithm algorithm, Encoding? encoding = null)
     {
         if (value == null)
-            throw new ArgumentNullException(nameof(value), "Null value to encrypt not suported.");
-
-        var inputBytes = Encoding.UTF8.GetBytes(value);
+            throw new ArgumentNullException(nameof(value), "Null value to encrypt not supported.");
+        
+        encoding ??= GlobalSettings.Properties.DefaultEncoding;
+        var inputBytes = encoding.GetBytes(value);
         var hashBytes = algorithm.ComputeHash(inputBytes);
+        return hashBytes.HashBytesToString();
+    }
+    
+    private static string HashBytesToString(this byte[] hashBytes)
+    {
         var hash = new StringBuilder();
         foreach (var b in hashBytes)
             hash.Append($"{b:x2}");
